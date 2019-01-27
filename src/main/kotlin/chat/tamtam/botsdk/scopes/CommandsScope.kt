8e86bdaf0340d315c0e852b5522f437424c14da8
alexander.melnikov <alexander.melnikov@corp.mail.ru>
2019-01-27 22:43:22 +0300

    Added new infix functions in CommandsScope and MessagesScope classes for sending messages.

--- src/main/kotlin/chat/tamtam/botsdk/scopes/CommandsScope.kt
+++ src/main/kotlin/chat/tamtam/botsdk/scopes/CommandsScope.kt
@@ -1,17 +1,21 @@
 package chat.tamtam.botsdk.scopes
 
+import chat.tamtam.botsdk.client.BotHttpManager
 import chat.tamtam.botsdk.model.Command
+import chat.tamtam.botsdk.model.UserId
+import chat.tamtam.botsdk.model.request.SendMessage
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @BotMarker
 class CommandsScope(
+    private val botHttpManager: BotHttpManager,
     private val log: Logger = LoggerFactory.getLogger(CommandsScope::class.java.name),
     private var unknownCommandAction: suspend (Command) -> Unit = {},
     private val commandsAnswers: MutableMap<String, suspend (Command) -> Unit> = mutableMapOf()
 ) {
 
-    operator fun get(command: String) = commandsAnswers[command] ?: unknownCommandAction
+    internal operator fun get(command: String) = commandsAnswers[command] ?: unknownCommandAction
 
     private fun saveCommand(command: String, action: suspend (Command) -> Unit) {
         log.info("saveCommand: start save command -> $command")
@@ -36,5 +40,13 @@ class CommandsScope(
     fun onUnknownCommand(action: suspend (Command) -> Unit) {
         saveUnknownCommand(action)
         log.info("onUnknownCommand: saved")
+    }
+
+    suspend infix fun Command.send(sendMessage: SendMessage) {
+        botHttpManager.sendMessage(UserId(this.update.message.sender.userId), sendMessage)
+    }
+
+    suspend infix fun Command.sendText(text: String) {
+        send(SendMessage(text))
     }
 }
--- src/main/kotlin/chat/tamtam/botsdk/scopes/MessagesScope.kt
+++ src/main/kotlin/chat/tamtam/botsdk/scopes/MessagesScope.kt
@@ -1,12 +1,35 @@
 package chat.tamtam.botsdk.scopes
 
-import chat.tamtam.botsdk.BotMarker
+import chat.tamtam.botsdk.client.BotHttpManager
+import chat.tamtam.botsdk.model.ChatId
+import chat.tamtam.botsdk.model.UserId
+import chat.tamtam.botsdk.model.request.SendMessage
 import chat.tamtam.botsdk.model.response.Message
 
 @BotMarker
-class MessagesScope {
+class MessagesScope(
+    private val botHttpManager: BotHttpManager,
+    private var messagesAnswer: suspend (Message) -> Unit = {}
+) {
 
-    fun answerOnMessage(answer: (callback: Message) -> Unit) {
+    fun answerOnMessage(answer: suspend (message: Message) -> Unit) {
+        messagesAnswer = answer
+    }
 
+    internal fun getAnswer() = messagesAnswer
+
+    suspend infix fun Message.send(sendMessage: SendMessage) {
+        when (this.recipient.chatId) {
+            -1L -> {
+                botHttpManager.sendMessage(UserId(this.recipient.chatId), sendMessage)
+            }
+            else -> {
+                botHttpManager.sendMessage(ChatId(this.recipient.chatId), sendMessage)
+            }
+        }
+    }
+
+    suspend infix fun Message.sendText(text: String) {
+        send(SendMessage(text))
     }
 }
