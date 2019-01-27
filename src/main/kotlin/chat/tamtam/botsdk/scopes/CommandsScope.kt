a0031c1ab6a76fb109bf9abbfa1483427b1e9ac6
alexander.melnikov <alexander.melnikov@corp.mail.ru>
2019-01-27 20:49:50 +0300

    Added suspend to lambdas in CommandsScope.

--- src/main/kotlin/chat/tamtam/botsdk/scopes/CommandsScope.kt
+++ src/main/kotlin/chat/tamtam/botsdk/scopes/CommandsScope.kt
@@ -1,6 +1,5 @@
 package chat.tamtam.botsdk.scopes
 
-import chat.tamtam.botsdk.BotMarker
 import chat.tamtam.botsdk.model.Command
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -8,33 +7,33 @@ import org.slf4j.LoggerFactory
 @BotMarker
 class CommandsScope(
     private val log: Logger = LoggerFactory.getLogger(CommandsScope::class.java.name),
-    private var unknownCommandAction: (Command) -> Unit = { _: Command -> },
-    private val commandsMap: MutableMap<Command, (Command) -> Unit> = mutableMapOf()
+    private var unknownCommandAction: suspend (Command) -> Unit = {},
+    private val commandsAnswers: MutableMap<String, suspend (Command) -> Unit> = mutableMapOf()
 ) {
 
-    operator fun get(command: Command) = commandsMap[command] ?: unknownCommandAction
+    operator fun get(command: String) = commandsAnswers[command] ?: unknownCommandAction
 
-    private fun saveCommand(command: Command, action: (Command) -> Unit) {
-        log.info("saveCommand: start save command -> ${command.name}")
-        commandsMap[command] = action
+    private fun saveCommand(command: String, action: suspend (Command) -> Unit) {
+        log.info("saveCommand: start save command -> $command")
+        commandsAnswers[command] = action
     }
 
-    private fun saveUnknownCommand(action: (Command) -> Unit) {
+    private fun saveUnknownCommand(action: suspend (Command) -> Unit) {
         log.info("saveCommand: start save unknown command")
         unknownCommandAction = action
     }
 
-    fun onStartCommand(action: (Command) -> Unit) {
-        saveCommand(Command("/start"), action)
+    fun onStartCommand(action: suspend (Command) -> Unit) {
+        saveCommand("/start", action)
         log.info("onStartCommand: saved")
     }
 
-    fun onCommand(command: Command, action: (Command) -> Unit) {
+    fun onCommand(command: String, action: suspend (Command) -> Unit) {
         saveCommand(command, action)
-        log.info("onCommand: saved command -> ${command.name}")
+        log.info("onCommand: saved command -> $command")
     }
 
-    fun onUnknownCommand(action: (Command) -> Unit) {
+    fun onUnknownCommand(action: suspend (Command) -> Unit) {
         saveUnknownCommand(action)
         log.info("onUnknownCommand: saved")
     }
