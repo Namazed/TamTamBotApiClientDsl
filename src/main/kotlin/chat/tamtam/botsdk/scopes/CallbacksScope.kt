058d4edd84390637eeaa249a7e36aeedff816e03
alexander.melnikov <alexander.melnikov@corp.mail.ru>
2019-01-27 20:51:44 +0300

    Configured CallbacksScope

--- src/main/kotlin/chat/tamtam/botsdk/scopes/CallbacksScope.kt
+++ src/main/kotlin/chat/tamtam/botsdk/scopes/CallbacksScope.kt
@@ -1,17 +1,26 @@
 package chat.tamtam.botsdk.scopes
 
-import chat.tamtam.botsdk.BotMarker
 import chat.tamtam.botsdk.model.Payload
 import chat.tamtam.botsdk.model.response.Callback
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @BotMarker
-class CallbacksScope {
+class CallbacksScope(
+    private val log: Logger = LoggerFactory.getLogger(CallbacksScope::class.java.name),
+    private var defaultAnswer: suspend (Callback) -> Unit = {},
+    private val callbacksAnswers: MutableMap<String, suspend (Callback) -> Unit> = mutableMapOf()
+) {
 
-    fun defaultAnswer(defaultAnswer: (callback: Callback) -> Unit) {
+    operator fun get(payloadButton: Payload) = callbacksAnswers[payloadButton.value] ?: defaultAnswer
 
+    fun defaultAnswer(defaultAnswer: suspend (callback: Callback) -> Unit) {
+        log.info("defaultAnswer: save")
+        this.defaultAnswer = defaultAnswer
     }
 
-    fun answerOnCallback(payloadButton: Payload, answer: (callback: Callback) -> Unit) {
-
+    fun answerOnCallback(payloadButton: Payload, answer: suspend (callback: Callback) -> Unit) {
+        log.info("answerOnCallback: save with payload ${payloadButton.value}")
+        callbacksAnswers[payloadButton.value] = answer
     }
 }
