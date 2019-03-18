bcf8dc0148fcc80021e4480e7f1cb53f45c4bb75
alexander.melnikov <alexander.melnikov@corp.mail.ru>
2019-03-18 10:29:46 +0300

    Improved requests error

--- library/src/main/kotlin/chat/tamtam/botsdk/model/response/Error.kt
+++ library/src/main/kotlin/chat/tamtam/botsdk/model/response/Error.kt
@@ -1,7 +1,9 @@
 package chat.tamtam.botsdk.model.response
 
 import kotlinx.serialization.Optional
+import kotlinx.serialization.Serializable
 
+@Serializable
 class Error(
     @Optional val error: String = "",
     val code: String,
