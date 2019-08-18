94c48d3d325099efce6d1c574433b54e68ef0ce1
alexander.melnikov <alexander.melnikov@corp.mail.ru>
2019-08-18 17:31:18 +0300

    fixes after merge master. Created starting params for webhook

--- library/src/main/kotlin/chat/tamtam/botsdk/communications/WebhookCommunication.kt
+++ library/src/main/kotlin/chat/tamtam/botsdk/communications/WebhookCommunication.kt
@@ -4,7 +4,6 @@ import chat.tamtam.botsdk.Coordinator
 import chat.tamtam.botsdk.UpdatesCoordinator
 import chat.tamtam.botsdk.client.HttpManager
 import chat.tamtam.botsdk.client.ResultRequest
-import chat.tamtam.botsdk.model.request.Subscription
 import chat.tamtam.botsdk.scopes.BotScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,8 +16,7 @@ class WebhookCommunication internal constructor(
     private val subscriptionScope: CoroutineScope = CoroutineScope(Dispatchers.IO),
     val log: Logger = LoggerFactory.getLogger(LongPollingCommunication::class.java.name)
 ) : Communication {
-
-    override fun start(botScope: BotScope, async: Boolean): Coordinator {
+    override fun start(botScope: BotScope, startingParams: StartingParams): Coordinator {
         subscribeIfNeeded(botScope)
         return UpdatesCoordinator(botScope)
     }
@@ -41,23 +39,26 @@ class WebhookCommunication internal constructor(
  * This class need for start webhook for your bot.
  */
 object webhook {
-    operator fun invoke(botToken: String, subscription: Subscription, init: BotScope.() -> Unit): Coordinator {
+    operator fun invoke(botToken: String, startingParams: StartingParams, init: BotScope.() -> Unit): Coordinator {
+        check(startingParams is WebhookStartingParams) {
+            "Wrong startingParams, for webhook you must use WebhookStartingParams"
+        }
         check(botToken.isNotEmpty()) { "Bot token must is not empty" }
-        check(subscription.url.isNotEmpty()) { "Your url must is not empty" }
+        check(startingParams.subscription.url.isNotEmpty()) { "Your url must is not empty" }
         val webHookCommunication = WebhookCommunication(botToken)
         val botHttpManager = HttpManager(webHookCommunication.botToken)
-        return webHookCommunication.init(botHttpManager, subscription, webHookCommunication.log, init)
+        return webHookCommunication.init(botHttpManager, startingParams, webHookCommunication.log, init)
     }
 }
 
 private fun Communication.init(
     botHttpManager: HttpManager,
-    subscription: Subscription,
+    startingParams: WebhookStartingParams,
     log: Logger,
     init: BotScope.() -> Unit
 ): Coordinator {
     log.info("Webhook bot starting")
-    val scope = BotScope(botHttpManager, subscription)
+    val scope = BotScope(botHttpManager, startingParams.subscription)
     scope.init()
-    return start(scope)
+    return start(scope, startingParams)
 }
